7bd6427a8331b27277920d575dea728ff9c031e7
khurt <khurtsidzeluka0@gmial.com>
2025-12-14 23:59:13 +0400

    User models created

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name='სახელი')
+    last_name = models.CharField(max_length=100, verbose_name='გვარი')
+    birth_date = models.DateField(verbose_name='დაბადების თარიღი')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def age(self):
+        today = date.today()
+        age = today.year - self.birth_date.year
+        if today.month < self.birth_date.month or \
+           (today.month == self.birth_date.month and today.day < self.birth_date.day):
+            age -= 1
+        return age
